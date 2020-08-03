58ff68981808f53129d51e7343532bbfc5c9234a
TBarry95 <x19141840@student.ncirl.ie>
2020-08-03 19:15:02 +0000

    aws scripts

---
+++ run_job_cluster.sh
@@ -0,0 +1,98 @@
+# !/bin/bash
+
+#############################################
+# Define working paths
+#############################################
+
+HDFS_PATH='/ssp_project'
+HDUSER_PATH='/home/hadoop/SSP'
+
+#############################################
+#
+#############################################
+
+#echo "Ensuring latest mapper and reducers are being used"
+
+# covid word count:
+# rm $HDUSER_PATH/mapper_clean3.py
+#rm $HDUSER_PATH/mapper_words.py
+#rm $HDUSER_PATH/reducer_words.py
+#cp $LOCAL_PATH/mapper_clean3.py $HDUSER_PATH
+#cp $LOCAL_PATH/mapper_words.py $HDUSER_PATH
+#cp $LOCAL_PATH/reducer_words.py $HDUSER_PATH
+
+# make executable
+chmod +x $HDUSER_PATH/mapper_clean3.py
+chmod +x $HDUSER_PATH/mapper_words.py
+chmod +x $HDUSER_PATH/reducer_words.py
+
+#############################################
+# Create folder structure on HDFS and copy input files from local file system
+#############################################
+
+echo "Initialising HDFS folders if not already set up"
+hdfs dfs -mkdir $HDFS_PATH
+hdfs dfs -mkdir $HDFS_PATH/input
+
+echo "Loading data to HDFS if not already present"
+hdfs dfs -copyFromLocal $HDUSER_PATH/get_tweets/combined_tweets_noheader.csv $HDFS_PATH/input
+
+#############################################
+# Delete output if exists: If  the output folder already exists, hadoop fails
+#############################################
+
+echo "Initialising local folders for storing Map Reduce outputs"
+hdfs dfs -rm $HDFS_PATH/output_job1/*
+hdfs dfs -rmdir $HDFS_PATH/output_job1
+hdfs dfs -rm $HDFS_PATH/output_job2/*
+hdfs dfs -rmdir $HDFS_PATH/output_job2
+
+#############################################
+# Run hadoop job 1:
+#############################################
+
+echo "Launching Hadoop Job 1: Preprocess and clean Twitter data"
+mapred streaming \
+-D mapred.reduce.tasks=0 \
+-file $HDUSER_PATH/mapper_clean3.py \
+-mapper 'python3 mapper_clean3.py' \
+-input $HDFS_PATH/input/combined_tweets_noheader.csv \
+-output $HDFS_PATH/output_job1
+
+#############################################
+# Run hadoop job 2:
+#############################################
+
+echo "Launching Hadoop Job 2: Aggregate the occurence of COVID in tweets"
+mapred streaming \
+-D mapred.reduce.tasks=1 \
+-file $HDUSER_PATH/mapper_words.py $HDUSER_PATH/reducer_words.py \
+-mapper 'python3 mapper_words.py' \
+-reducer 'python3 reducer_words.py' \
+-input $HDFS_PATH/output_job1/part-00000 \
+-input $HDFS_PATH/output_job1/part-00001 \
+-output $HDFS_PATH/output_job2
+
+#############################################
+# Copy output files to local file system
+#############################################
+
+echo "Copying final output from HDFS to local folder"  
+rm $HDUSER_PATH/output/job_1/*
+rmdir $HDUSER_PATH/output/job_1
+mkdir $HDUSER_PATH/output/job_1
+
+rm $HDUSER_PATH/output/job_2/*
+rmdir $HDUSER_PATH/output/job_2
+mkdir $HDUSER_PATH/output/job_2
+
+hdfs dfs -copyToLocal $HDFS_PATH/output_job1/* $HDUSER_PATH/output/job_1
+hdfs dfs -copyToLocal $HDFS_PATH/output_job2/* $HDUSER_PATH/output/job_2
+
+#############################################
+# Stop Hadoop services
+#############################################
+
+#$HADOOP_PATH/sbin/stop-yarn.sh
+#$HADOOP_PATH/sbin/stop-dfs.sh
+
