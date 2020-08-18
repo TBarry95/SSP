eb0b877740ebfd69907c71d68db0feebf127bc0c
Tiernan Barry <x19141840@student.ncirl.ie>
2020-08-18 20:51:29 +0100

    Add files via upload

---
+++ bootstrap_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+
+#############################################
+# Install packages
+#############################################
+
+sudo yum update -y
+sudo yum install git -y 
+sudo pip3 install spacy 
+sudo python3 -m spacy download en_core_web_sm 
+sudo pip3 install pandas 
+sudo pip3 install textblob 
+sudo pip3 install scipy
+sudo pip3 install sortedcontainers
