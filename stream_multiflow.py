390687ead2d0e8fd02e4870399ecb721dca3b62d
Tiernan Barry <x19141840@student.ncirl.ie>
2020-08-20 00:34:43 +0100

    stream

--- stream_multiflow.py
+++ stream_multiflow.py
@@ -27,27 +27,23 @@ model = HoeffdingTreeRegressor()
 correctness_dist = []
 
 for line in csv.reader(sys.stdin):
-    try:
+    if len(line) == 14 and line[0] != 'DATE_TIME' and line[0][0:4] == "2020":
 
-        if len(line) == 14 and line[0] != 'DATE_TIME' and line[0][0:4] == "2020":
-            df = pd.DataFrame()
-            df['covid'] = [float(line[13])]
-            df['sent'] = [float(line[3])]
-            #print(df)
-            stream = DataStream(df)
-            #X,Y = stream.current_sample_x, stream.current_sample_y
-            X, Y = stream.next_sample()
+        df = pd.DataFrame()
+        df['covid'] = [float(line[13])]
+        df['sent'] = [float(line[3])]
+        stream = DataStream(df)
+        iters = 1
 
-            # covid = X, sent = Y
-            # covid, sent  = float(line[13]), float(line[3])
-            # data = tuple([np.array([[covid]]), np.array([sent])])
+        # X,Y = stream.current_sample_x, stream.current_sample_y
+        X, Y = stream.next_sample()
 
-            prediction = model.predict(X)
-            if Y == prediction:
-                correctness_dist.append(1)
-            else:
-                correctness_dist.append(0)
-            model.partial_fit(X,Y)
-            print("Predictions vs Actual:", prediction,Y)
-    except IndexError:
-        continue
+        prediction = model.predict(X)
+        if Y == prediction:
+            correctness_dist.append(1)
+        else:
+            correctness_dist.append(0)
+        model.partial_fit(X, Y)
+        print("Predictions vs Actual:", prediction, Y)
+
+
--- stream_predictions.py
+++ stream_predictions.py
@@ -8,26 +8,37 @@ data = data[data['DATE_TIME'].str.startswith("2020")]
 del data['DATE_TIME']
 del data[' SOURCE']
 data = data[[' MEAN_SENT_CATG', ' COVID_COUNT\t']]
+data.columns = ['MEAN_SENT_CATG', 'COVID_COUNT']
 
 #data.to_csv("/home/tiernan/Documents/test_stream.csv", index=False)
 #stream = FileStream("/home/tiernan/Documents/test_stream.csv", allow_nan = True)
 #test = stream.next_sample()
 
-stream = DataStream(data)
-
 from skmultiflow.trees import HoeffdingTree
 tree = HoeffdingTree()
 
 correctness_dist = []
-nb_iters = 1
 
-for i in range(nb_iters):
-    X, Y = stream.next_sample()
+for i in range(len(data)):
 
-    prediction = tree.predict(X)  # predict Y using the tree
-    if Y == prediction:                # check the prediction
-        correctness_dist.append(1)
-    else:
-        correctness_dist.append(0)
+    df = pd.DataFrame()
+    df['covid'] = data['COVID_COUNT'][i]
+    df['SENT_CATG'] = data['MEAN_SENT_CATG'][i]
 
-    tree.partial_fit(X, Y)  # update the tree
+    print(df)
+
+    '''nb_iters = 1
+    stream = DataStream(data.head(1))
+
+    for i in range(nb_iters):
+
+        X, Y = stream.next_sample()
+
+        prediction = tree.predict(X)  # predict Y using the tree
+        if Y == prediction:                # check the prediction
+            correctness_dist.append(1)
+        else:
+            correctness_dist.append(0)
+
+        tree.partial_fit(X, Y)  # update the tree
+        print("Prediction v ActuaL: ", prediction, Y)'''
